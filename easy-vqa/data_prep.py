14f847279f7f17f3618e055719d7c64770ae35a8
volodyatan <volodyatan@yahoo.ca>
2021-12-08 12:01:53 -0500

    changing data_prep.py return

--- easy-vqa/data_prep.py
+++ easy-vqa/data_prep.py
@@ -13,57 +13,57 @@ QUESTIONS_PATH = './data/questions.JSON'
 ANSWERS_PATH = './data/answers.txt'
 IMAGES_PATH = './data/images/'
 
-def data_prep():
-    # open questions
-    with open(QUESTIONS_PATH, 'r') as file:
-        qs = json.load(file)
-    file.close()
-    # go through the questions file and get the questions, answers, and ids
-    questions = []
-    ans = []
-    ids = []
+# open questions
+with open(QUESTIONS_PATH, 'r') as file:
+    qs = json.load(file)
+file.close()
+# go through the questions file and get the questions, answers, and ids
+questions = []
+ans = []
+ids = []
 
-    for q in qs:
-        questions.append(qs[q][0])
-        ans.append(qs[q][1])
-        ids.append(qs[q][2])
+for q in qs:
+    questions.append(qs[q][0])
+    ans.append(qs[q][1])
+    ids.append(qs[q][2])
 
-    # open answers file and get all possible answers
-    all_ans = []
-    total_ans = 0
-    with open(ANSWERS_PATH, 'r') as file:
-        for a in file:
-            all_ans.append(a.strip())
-            total_ans+=1
-    file.close()
+# open answers file and get all possible answers
+all_ans = []
+total_ans = 0
+with open(ANSWERS_PATH, 'r') as file:
+    for a in file:
+        all_ans.append(a.strip())
+        total_ans+=1
+file.close()
 
-    # map image ids to image paths
-    image_paths = {}
-    for image in os.listdir(IMAGES_PATH):
-        img_id = int(image.split('.')[0])
-        image_paths[img_id] = IMAGES_PATH+str(image)
+# map image ids to image paths
+image_paths = {}
+for image in os.listdir(IMAGES_PATH):
+    img_id = int(image.split('.')[0])
+    image_paths[img_id] = IMAGES_PATH+str(image)
 
-    # map image ids to processed images
-    images = {}
-    for i in image_paths:
-        # load image using keras
-        ld_img = load_img(image_paths[i])
-        # image to array using keras
-        img_arr = img_to_array(ld_img)
-        # shift pixel values
-        shift_img = img_arr/255-.5
-        # save img to images object
-        images[i] = shift_img
-    # get images shape 
-    # image_shape = images[0].shape
+# map image ids to processed images
+images = {}
+for i in image_paths:
+    # load image using keras
+    ld_img = load_img(image_paths[i])
+    # image to array using keras
+    img_arr = img_to_array(ld_img)
+    # shift pixel values
+    shift_img = img_arr/255-.5
+    # save img to images object
+    images[i] = shift_img
+# get images shape 
+image_shape = images[0].shape
 
+def get_questions():
     # get all unique words in questions
     words = set('')
     for q in questions:
         # remove question mark
         q = q[:-1]
         words.update(q.split())
-    words_len = len(words)
+    num_words = len(words)
 
     # tokenize words
     tokens = {}
@@ -78,8 +78,6 @@ def data_prep():
     questions_bow = np.zeros((len(questions),len(tokens)))
     count = 0
     for q in questions:
-        # bag of words for this question
-        bow = []
         # remove questions mark
         q = q[:-1]
         # turn into list
@@ -90,32 +88,39 @@ def data_prep():
                 # if it is, set np array of index count x id of t in token to 1
                 questions_bow[count][tokens[t]] = 1.
         count += 1
+    return questions_bow, num_words
 
-    # create model inputs
-    x = np.array([images[id] for id in ids])
+# get questions
+all_questions, num_words = get_questions()
 
-    # create model outputs
-    answers_idx = [all_ans.index(a) for a in ans]
-    y = to_categorical(answers_idx)
+# create model inputs
+x = np.array([images[id] for id in ids])
 
-    # zip and shuffle
+# create model outputs
+answers_idx = [all_ans.index(a) for a in ans]
+y = to_categorical(answers_idx)
 
-    xy = list(zip(x, y)) ## zip to keep data and labels aligned during shuffle
+# # zip and shuffle
+# xyq = list(zip(x, y, all_questions)) ## zip to keep data and labels aligned during shuffle
+# seed(13) ## seed for reproducibility
+# shuffle(xyq)
+# x, y, all_questions = zip(*xyq) ## unzip shuffled data
 
-    seed(13) ## seed for reproducibility
+# set ratio for train
+ratio = 0.8
+# split x into train and test
+ratio = math.floor(len(x)*ratio)
+x_train = x[:ratio]
+x_test = x[ratio:]
+# split x into train and test
+y_train = y[:ratio]
+y_test = y[ratio:]
+# split questions
+train_questions = all_questions[:ratio]
+test_questions = all_questions[ratio:]
+# split answers
+train_answers = all_ans[:ratio]
+test_answers = all_ans[ratio:]
 
-    shuffle(xy)
-
-    x, y = zip(*xy) ## unzip shuffled data
-
-    # set ratio for train
-    ratio = 0.8
-    # split x into train and test
-    ratio = math.floor(len(x)*ratio)
-    x_train = x[:ratio]
-    x_test = x[ratio:]
-    # split x into train and test
-    y_train = y[:ratio]
-    y_test = y[ratio:]
-
-    return (x_train, x_test, y_train, y_test)
+def get_xy():
+    return (x_train, x_test, y_train, y_test, train_questions, test_questions, train_answers, test_answers, num_words, image_shape)
